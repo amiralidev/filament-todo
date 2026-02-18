85043f79dc6752ecd854ec6bcb15bcf4b88db577
Amirali Kalantari Pour <amiralidev@gmail.com>
2026-02-18 10:43:24 +0330

    Update TodoBoardWidget.php

--- src/Widgets/TodoBoardWidget.php
+++ src/Widgets/TodoBoardWidget.php
@@ -7,9 +7,9 @@ use Filament\Widgets\Widget;
 
 class TodoBoardWidget extends Widget
 {
-    protected static string $view = 'filament-todo::filament.widgets.todo-board-widget';
+    protected string $view = 'filament-todo::filament.widgets.todo-board-widget';
 
-    protected int | string | array $columnSpan = 'full';
+    protected int|string|array $columnSpan = 'full';
 
     public function mount(): void
     {
